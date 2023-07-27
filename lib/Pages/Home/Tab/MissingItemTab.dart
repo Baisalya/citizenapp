2c1da10daf1a6cc0123d5e02272a14eb3d8be0ca
Baisalya <baishalya1999@gmail.com>
2023-07-27 16:15:13 +0530

    tabs only classes added

---
+++ lib/Pages/Home/Tab/MissingItemTab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MissingItemTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Missing Item Screen Content"),
+    );
+  }
+}
---
+++ lib/Pages/Home/Tab/PoliceTab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PoliceTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Police Screen Content"),
+    );
+  }
+}
